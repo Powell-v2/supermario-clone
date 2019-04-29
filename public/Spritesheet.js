cbe2a10d3a9f0d14a4c6462509e30d81366a012f
Paul Ermolin <oatto.paul@gmail.com>
2019-04-29 18:09:54 +0100

    refactor: Spritesheet

--- public/Spritesheet.js
+++ public/Spritesheet.js
@@ -1,3 +1,4 @@
+`use strict`
 class Spritesheet {
   constructor(img, width, height) {
     this.img = img
@@ -6,23 +7,27 @@ class Spritesheet {
     this.tileset = new Map()
   }
 
-  define(name, x, y) {
+  define(name, x, y, width, height) {
     const buffer = document.createElement(`canvas`)
 
-    buffer.width = this.width
-    buffer.height = this.height
+    buffer.width = width
+    buffer.height = height
 
     buffer
       .getContext(`2d`)
       .drawImage(
         this.img,
-        x * this.width, y * this.height,
-        this.width, this.height,
+        x, y,
+        width, height,
         0, 0,
-        this.width, this.height,
+        width, height,
       )
 
     this.tileset.set(name, buffer)
+  }
+
+  defineTile(name, x, y) {
+    this.define(name, x * this.width, y * this.height, this.width, this.height)
   }
 
   draw(name, ctx, x, y) {
