4f3b14a7bd01b6b0f62cc892275df2b7923b90e5
Paul Ermolin <oatto.paul@gmail.com>
2019-04-29 18:10:13 +0100

    feat: define methods for creating layers

---
+++ public/layers.js
@@ -0,0 +1,35 @@
+`use strict`
+const drawBackground = (bg, ctx, sprites) => {
+  bg.ranges.forEach(({ x: [x1, x2], y: [y1, y2] }) => {
+    for (let x = x1; x < x2; x += 1) {
+      for (let y = y1; y < y2; y += 1) {
+        sprites.drawTile(bg.tile, ctx, x, y)
+      }
+    }
+  })
+}
+
+const createBackgroundLayer = (backgrounds, sprites) => {
+  const buff = document.createElement(`canvas`)
+  buff.width = 256
+  buff.height = 240
+
+  backgrounds.forEach((bg) => {
+    drawBackground(bg, buff.getContext(`2d`), sprites)
+  })
+
+  return function drawBackgroundLayer(ctx) {
+    ctx.drawImage(buff, 0, 0)
+  }
+}
+
+const createSpriteLayer = (sprite, pos) => {
+  return function drawSpriteLayer(ctx) {
+    sprite.draw(`idle`, ctx, pos.x, pos.y)
+  }
+}
+
+export {
+  createBackgroundLayer,
+  createSpriteLayer,
+}
