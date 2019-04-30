823961185cf1f0a136980323f69ae916305307b8
Paul Ermolin <oatto.paul@gmail.com>
2019-04-30 12:06:58 +0100

    feat: make Mario jump

--- public/main.js
+++ public/main.js
@@ -1,31 +1,43 @@
 `use strict`
-import Spritesheet from './Spritesheet.js'
-import { loadImage, loadLevel } from './loaders.js'
+import Compositor from './Compositor.js'
+import Entity from './Entity.js'
+import Timer from './Timer.js'
 
-const drawBackground = (bg, ctx, sprites) => {
-  bg.ranges.forEach(({ x: [x1, x2], y: [y1, y2] }) => {
-    for (let x = x1; x < x2; x += 1) {
-      for (let y = y1; y < y2; y += 1) {
-        sprites.drawTile(bg.tile, ctx, x, y)
-      }
-    }
-  })
-}
+import { createMario } from './entities.js'
+import { createBackgroundLayer, createSpriteLayer } from './layers.js'
+import { loadLevel } from './loaders.js'
+import { loadBackgroundSprites } from './sprites.js'
 
 const scene = document.getElementById(`scene`)
 const ctx = scene.getContext(`2d`)
 
-const loadBackgroundSprites = () => {
-  return loadImage(`/assets/tileset.png`)
-    .then((img) => {
-      const sprites = new Spritesheet(img, 16, 16)
-      sprites.define(`ground`, 0, 0)
-      sprites.define(`sky`, 3, 23)
-      return sprites
-  })
-}
+Promise.all([
+  createMario(),
+  loadBackgroundSprites(),
+  loadLevel(`1-1`)
+])
+  .then(([ mario, bgSprites, lvl ]) => {
+    const comp = new Compositor()
+    const bgLayer = createBackgroundLayer(lvl.backgrounds, bgSprites)
+    comp.layers.push(bgLayer)
 
-Promise.all([loadBackgroundSprites(), loadLevel(`1-1`)])
-  .then(([ sprites, lvl ]) => {
-    lvl.backgrounds.forEach((bg) => drawBackground(bg, ctx, sprites))
+    const gravity = 1500
+
+    mario.pos.set(64, 180)
+    mario.vel.set(150, -500)
+
+    const spriteLayer = createSpriteLayer(mario)
+    comp.layers.push(spriteLayer)
+
+    const timer = new Timer()
+
+    timer.update = function(delta) {
+      mario.update(delta)
+      
+      comp.draw(ctx)
+
+      mario.vel.y += gravity * delta
+    }
+
+    timer.start(0)
   })
