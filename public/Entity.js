a98c4455c4f0a9b8bda2e5f935c137c136ee7203
Paul Ermolin <oatto.paul@gmail.com>
2019-04-30 14:21:31 +0100

    feat: define Trait class and keep track of traits in Entity

--- public/Entity.js
+++ public/Entity.js
@@ -10,10 +10,31 @@ class Vector2 {
   }
 }
 
+export class Trait {
+  constructor(name) {
+    this.NAME = name
+  }
+
+  update() {
+    console.warn(`Trait: unhandled update call.`)
+  }
+}
+
 class Entity {
   constructor() {
     this.pos = new Vector2(0, 0)
     this.vel = new Vector2(0, 0)
+
+    this.traits = []
+  }
+
+  addTrait(trait) {
+    this.traits.push(trait)
+    this[trait.NAME] = trait
+  }
+
+  update(delta) {
+    this.traits.forEach((trait) => trait.update(this, delta))
   }
 }
 
