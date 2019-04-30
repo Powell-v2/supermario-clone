2c09a3f8ab9f32b755ceaec8c48572f1c4e26bc2
Paul Ermolin <oatto.paul@gmail.com>
2019-04-30 12:06:00 +0100

    feat: define Timer class

---
+++ public/Timer.js
@@ -0,0 +1,30 @@
+class Timer {
+  constructor(delta = 1/60) {
+    let accumulatedTime = 0
+    let lastTimestamp = 0
+
+    this.updateProxy = (timestamp) => {
+      accumulatedTime += (timestamp - lastTimestamp) / 1000
+
+      while (accumulatedTime > delta) {
+        this.update(delta)
+
+        accumulatedTime -= delta
+      }
+
+      lastTimestamp = timestamp
+
+      this.enqueue()
+    }
+  }
+
+  enqueue() {
+    requestAnimationFrame(this.updateProxy)
+  }
+
+  start() {
+    this.enqueue()
+  }
+}
+
+export default Timer
