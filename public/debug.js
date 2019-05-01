039a16321d35bb3b6ee8984badd3ff2563def6b6
Paul Ermolin <oatto.paul@gmail.com>
2019-05-01 16:00:52 +0100

    feat: set up debugging with mouse

---
+++ public/debug.js
@@ -0,0 +1,34 @@
+const setupMouseControl = (scene, ent, cam) => {
+  const events = [`mousedown`, `mousemove`]
+  let lastEvent
+
+  events.forEach((type) => {
+    scene.addEventListener(type, (ev) => {
+      const { offsetX, offsetY, buttons } = ev
+
+      if (buttons === 1) {
+        ent.vel.set(0, 0)
+        ent.pos.set(
+          offsetX + cam.pos.x,
+          offsetY + cam.pos.y
+        )
+      }
+      else if (
+        buttons === 2 &&
+        lastEvent &&
+        lastEvent.buttons === 2 &&
+        lastEvent.type === `mousemove`
+      ) {
+        cam.pos.x -= offsetX - lastEvent.offsetX
+      }
+
+      lastEvent = ev
+    })
+  })
+
+  scene.addEventListener(`contextmenu`, (ev) => ev.preventDefault())
+}
+
+export {
+  setupMouseControl,
+}
